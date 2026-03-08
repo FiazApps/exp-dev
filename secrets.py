3b9b6acc63905009bc36378da2eca9a988cdb9bb
FiazApps <Fiazakram1@gmail.com>
2026-03-08 19:57:20 +0000

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,6 @@
+WIFI_SSID = "your_wifi_name"
+WIFI_PASSWORD = "your_wifi_password"
+
+# OpenWeatherMap example (free tier)
+WEATHER_API_KEY = "your_api_key"
+CITY_ID = "your_city_id"  # e.g., 2643743 for London
